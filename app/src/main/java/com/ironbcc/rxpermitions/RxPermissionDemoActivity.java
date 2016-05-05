3afaa623eabbaf7a1b7dc8289f85bdc4420c1481
ironbcc <ironbcc@yandex-team.ru>
2016-05-05 20:14:07 +0300

    added sample for dynamic permission

--- app/src/main/java/com/ironbcc/rxpermitions/RxPermissionDemoActivity.java
+++ app/src/main/java/com/ironbcc/rxpermitions/RxPermissionDemoActivity.java
@@ -1,8 +1,18 @@
 package com.ironbcc.rxpermitions;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -12,6 +22,8 @@ import android.view.MenuItem;
 public class RxPermissionDemoActivity
     extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1234;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +32,99 @@ public class RxPermissionDemoActivity
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
+                onFabClick();
             }
         });
+    }
+
+    private void onFabClick() {
+        showContactList();
+    }
+
+    private void showContactList() {
+        if(tryToRequestPermissions()) {
+            ContentResolver cr = getContentResolver();
+            Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                                  null, null, null, null);
+            new AlertDialog.Builder(this)
+                .setAdapter(
+                    new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cur, new String[] {ContactsContract.Contacts.DISPLAY_NAME}, new int[]{android.R.id.text1}, 0),
+                    null
+                )
+                .show();
+        }
+    }
+
+    private boolean tryToRequestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                final AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setView(R.layout.dialog_contact_permition_explanation)
+                    .create();
+                dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions();
+                    }
+                });
+                dialog.show();
+            } else {
+                // No explanation needed, we can request the permission.
+                requestPermissions();
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            new String[]{Manifest.permission.READ_CONTACTS}, MY_PERMISSIONS_REQUEST_READ_CONTACTS
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    Snackbar.make(findViewById(R.id.fab), "Permission not granted! You son of the bitch!!!", Snackbar.LENGTH_LONG)
+                        .setAction("Grant", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                showContactList();
+                            }
+                        }).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override

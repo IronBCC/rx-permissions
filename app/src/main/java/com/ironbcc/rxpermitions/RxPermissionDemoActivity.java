13396ac33f535ef2fa2910eed9faec60557ba8a8
ironbcc <ironbcc@yandex-team.ru>
2016-05-06 17:49:56 +0300

    cleaned up demo activity

--- app/src/main/java/com/ironbcc/rxpermitions/RxPermissionDemoActivity.java
+++ app/src/main/java/com/ironbcc/rxpermitions/RxPermissionDemoActivity.java
@@ -23,8 +23,6 @@ import rx.functions.Action1;
 public class RxPermissionDemoActivity
     extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1234;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,22 +58,7 @@ public class RxPermissionDemoActivity
                 @Override
                 public void call(Boolean granted) {
                     if (granted) {
-
-                        ContentResolver cr = getContentResolver();
-                        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
-                                              null, null, null, null
-                        );
-                        new AlertDialog.Builder(RxPermissionDemoActivity.this)
-                            .setAdapter(
-                                new SimpleCursorAdapter(
-                                    RxPermissionDemoActivity.this,
-                                    android.R.layout.simple_list_item_1,
-                                    cur,
-                                    new String[]{ContactsContract.Contacts.DISPLAY_NAME}, new int[]{android.R.id.text1}, 0
-                                ),
-                                null
-                            )
-                            .show();
+                        showContactListUnsecure();
                     } else {
                         Snackbar.make(findViewById(R.id.fab), "Permission not granted! You son of the bitch!!!", Snackbar.LENGTH_LONG)
                             .setAction("Grant", new View.OnClickListener() {
@@ -90,47 +73,25 @@ public class RxPermissionDemoActivity
             .subscribe();
     }
 
-    private boolean tryToRequestPermissions() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                final AlertDialog dialog = new AlertDialog.Builder(this)
-                    .setView(R.layout.dialog_contact_permition_explanation)
-                    .create();
-                dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                    @Override
-                    public void onDismiss(DialogInterface dialog) {
-                        requestPermissions();
-                    }
-                });
-                dialog.show();
-            } else {
-                // No explanation needed, we can request the permission.
-                requestPermissions();
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-            return false;
-        }
-        return true;
-    }
-
-    private void requestPermissions() {
-        ActivityCompat.requestPermissions(
-            this,
-            new String[]{Manifest.permission.READ_CONTACTS}, MY_PERMISSIONS_REQUEST_READ_CONTACTS
+    private void showContactListUnsecure() {ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                              null, null, null, null
         );
+        new AlertDialog.Builder(RxPermissionDemoActivity.this)
+            .setAdapter(
+                new SimpleCursorAdapter(
+                    RxPermissionDemoActivity.this,
+                    android.R.layout.simple_list_item_1,
+                    cur,
+                    new String[]{ContactsContract.Contacts.DISPLAY_NAME}, new int[]{android.R.id.text1}, 0
+                ),
+                null
+            )
+            .show();
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         RxPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
